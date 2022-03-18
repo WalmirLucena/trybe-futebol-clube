3f935f7ae387151f3c6e74c23c2074815142f855
WalmirLucena <walmirlucena3@gmail.com>
2022-03-18 14:59:55 -0400

    Muda .env

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -1,13 +1,13 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
 import chaiHttp = require('chai-http');
+
 import { app } from '../app';
 
-import User from '../database/models/User';/* 
-import { describe } from 'mocha'; */
+import User from '../database/models/User';
 import userMock from '../mock/model/userMock';
 
-import { Response, Request } from 'superagent';
+import { Response } from 'superagent';
 
 chai.use(chaiHttp);
 
@@ -15,12 +15,14 @@ const { expect } = chai;
 
 const mock = [
   {
-    username: 'Admin',
-    role: 'admin',
-    email: 'admin@admin.com',
-    password: 'secret_admin',
+    id: 1,
+    username: 'User',
+    role: 'user',
+    email: 'user@user.com',
+    password: 'secret_user',
   },
   {
+    id:3,
     username: 'Rubao',
     role: 'rubao',
     email: 'rubao@user.com',
@@ -33,13 +35,7 @@ describe('Testando a Rota /login', () => {
   let chaiHttpResponse: Response;
 
   before(async () => {
-    sinon.stub(User, 'findOne').resolves({
-      id: 1,
-      username: 'User',
-      role: 'user',
-      email: 'user@user.com',
-      password: 'secret_user',
-    } as User);
+    sinon.stub(User, 'findOne').resolves( mock[0] as User);
   });
 
   after(() => {
@@ -47,30 +43,30 @@ describe('Testando a Rota /login', () => {
   });
 
   it("Quando o login é feito com sucesso", async () => {
-    chaiHttpResponse = await chai.request(app)
+    chaiHttpResponse = await chai
+    .request(app)
     .post('/login')
     .send({
-      username: mock[0].username,
+      email: mock[0].email,
       password: mock[0].password
     });
-    /* .end(async function (err, res) {
-      expect(res).to.have.status(200)    }); */
+
     console.log(chaiHttpResponse);
     
 
-    expect(chaiHttpResponse).to.be.eq(404);
+    expect(chaiHttpResponse).to.have.status(200);
   })
 
   it("Quando o email ou senha é invalido", async ()=> {
     chaiHttpResponse = await chai.request(app)
     .post('/login')
     .send({
-      username: mock[1].username,
+      email: mock[1].email,
       password: mock[1].password
     })
     .end( function (err, response) {
       expect(response).to.have.status(401)
-      expect('/home')    });
+    });
 
    /*  expect(chaiHttpResponse).to.be.eq(401); */
   })
