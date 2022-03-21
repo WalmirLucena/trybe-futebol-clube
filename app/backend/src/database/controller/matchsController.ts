4b171b1c0ea9691e4510f0bd5c208dc44839af68
WalmirLucena <walmirlucena3@gmail.com>
2022-03-21 08:26:04 -0400

    Altera rota matchs para passar testes

--- app/backend/src/database/controller/matchsController.ts
+++ app/backend/src/database/controller/matchsController.ts
@@ -2,15 +2,26 @@ import { Request, Response } from 'express';
 import matchsService from '../services/matchsService';
 import StatusCode from '../Utils/StatusCode';
 
-const getAll = async (req: Request, res: Response): Promise<Response> => {
+const getByQuery = async (req: Request, res: Response, inProgress: string) => {
+  let query;
+
+  if (inProgress === 'true') query = true;
+  else query = false;
+
+  const result = await matchsService.getByQuery(query);
+
+  return res.status(StatusCode.OK).json(result);
+};
+
+const getAll = async (req: Request, res: Response) => {
   try {
-    let result;
-    if (req.query) {
-      result = await matchsService.getByQuery(Boolean(req.query.inProgress));
+    const { inProgress } = req.query;
+    if (!inProgress) {
+      const result = await matchsService.getAll();
       return res.status(StatusCode.OK).json(result);
     }
-    result = await matchsService.getAll();
-    return res.status(StatusCode.OK).json(result);
+
+    getByQuery(req, res, String(inProgress));
   } catch (err) {
     return res.status(StatusCode.NOT_FOUND)
       .json({ error: `${err}` });
--- app/backend/src/database/services/matchsService.ts
+++ app/backend/src/database/services/matchsService.ts
@@ -1,27 +1,27 @@
 import Clubs from '../models/Clubs';
 import Matchs from '../models/Matchs';
 
+const getByQuery = async (query: boolean) => {
+  const matchs = await Matchs.findAll({
+    where: { inProgress: query },
+    attributes: { exclude: ['home_team', 'away_team'] },
+    include: [
+      { model: Clubs, as: 'awayClub', attributes: ['clubName'] },
+      { model: Clubs, as: 'homeClub', attributes: ['clubName'] },
+    ],
+  });
+
+  return matchs;
+};
+
 const getAll = async () => {
   const matchs = await Matchs.findAll({
-    attributes: { exclude: ['home_team', 'away_team'] },
     include: [
-      { model: Clubs, as: 'awayClub', attributes: [['club_name', 'clubName']] },
-      { model: Clubs, as: 'homeClub', attributes: [['club_name', 'clubName']] },
+      { model: Clubs, as: 'awayClub', attributes: ['clubName'] },
+      { model: Clubs, as: 'homeClub', attributes: ['clubName'] },
     ],
   });
   return matchs;
 };
 
-const getByQuery = async (inProgress: boolean) => {
-  const matchs = await Matchs.findAll({
-    where: { inProgress },
-    attributes: { exclude: ['home_team', 'away_team'] },
-    include: [
-      { model: Clubs, as: 'awayClub', attributes: [['club_name', 'clubName']] },
-      { model: Clubs, as: 'homeClub', attributes: [['club_name', 'clubName']] },
-    ],
-  });
-
-  return matchs;
-};
 export default { getAll, getByQuery };
